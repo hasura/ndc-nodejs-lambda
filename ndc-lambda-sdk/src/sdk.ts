eac40ed906d66cd600f04e90382a706d5898feb9
Daniel Chambers <daniel@hasura.io>
2024-02-26 12:47:04 +1100

    Add tracing documentation and export tracing functions (#16)
    * Add tracing documentation and export tracing functions
    
    * Update changelog

--- ndc-lambda-sdk/src/sdk.ts
+++ ndc-lambda-sdk/src/sdk.ts
@@ -1,3 +1,4 @@
 export { JSONValue } from "./schema";
 export { ConnectorError, BadRequest, Forbidden, Conflict, UnprocessableContent, InternalServerError, NotSupported, BadGateway } from "@hasura/ndc-sdk-typescript";
+export { withActiveSpan, USER_VISIBLE_SPAN_ATTRIBUTE } from "@hasura/ndc-sdk-typescript/instrumentation"
 export { ErrorDetails, getErrorDetails } from "./execution";
