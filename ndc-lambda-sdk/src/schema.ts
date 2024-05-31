be4105f72bd93be0bffe97e45ba69c6fc0b35a94
Daniel Chambers <daniel@hasura.io>
2024-05-31 11:11:24 +1000

    Update to TS SDK v5, support biginteger representation

--- ndc-lambda-sdk/src/schema.ts
+++ ndc-lambda-sdk/src/schema.ts
@@ -298,7 +298,7 @@ function convertBuiltInScalarTypeIntoSdkSchemaType(typeName: BuiltInScalarTypeNa
       comparison_operators: { "_eq": { type: "equal" } },
     };
     case BuiltInScalarTypeName.BigInt: return {
-      representation: { type: "int64" }, // NDC doesn't have a good representation for this type as at v0.1.2, so this is the best representation in the meantime
+      representation: { type: "biginteger" },
       aggregate_functions: {},
       comparison_operators: { "_eq": { type: "equal" } },
     };
