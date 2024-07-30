957b2efc4840787aed86f5ad25d906d22acd3e82
Daniel Chambers <daniel@hasura.io>
2024-07-30 12:19:44 +1000

    Delete unused yeoman generator

--- yeoman-generator/src/app/templates/functions.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @readonly Exposes the function as an NDC function (the function should only query data without making modifications)
- */
-export function hello(name?: string) {
-  return `hello ${name ?? "world"}`;
-}
